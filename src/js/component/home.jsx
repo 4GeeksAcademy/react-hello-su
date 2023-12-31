1507e7a9cc452b3fc8e598fe006d8da2ca75a22d
Suri1336 <146222294+Suri1336@users.noreply.github.com>
2023-12-31 02:36:27 +0000

    completed

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -6,12 +6,15 @@ import rigoImage from "../../img/rigo-baby.jpg";
 //create your first component
 const Home = (props) => {
 	return (
-		<div className="d-flex">
-			<h1>{Math.floor(props.second / 1000000)% 10}</h1>
+		<div> 
+			<h2 class="header">My Counter</h2>
+		<div className=" timer d-flex justify-content-center ">
 			<h1>{Math.floor(props.second / 100000)% 10}</h1>
 			<h1>{Math.floor(props.second / 10000)% 10}</h1>
 			<h1>{Math.floor(props.second / 1000)% 10}</h1>
 			<h1>{Math.floor(props.second / 100)% 10}</h1>
+			<h1>{Math.floor(props.second / 10)% 10}</h1>
+		</div>
 		</div>
 	);
 };
